e1fc44fd543b439fe238386af9973314a9926900
jazmin <tegan01sara12@gmail.com>
2021-05-21 13:18:02 -0500

    Varios de premain

--- react-katth/mai.js
+++ react-katth/mai.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 function App() {
 
-    // eslint-disable-next-line no-unused-vars
+    
     let getData = async() => {
       const url = `http://localhost:8000/clothing`
       const getFetchData = await fetch(url).then(result => result.json())
