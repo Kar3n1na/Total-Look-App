af39c6fbf8d3aac8d9eb4f3ce21735f1214b06b9
Brenda García <brendagar88@gmail.com>
2021-05-21 01:25:04 -0500

    Muestra todos los productos en un array. Función en main

--- react-katth/src/App.js
+++ react-katth/src/App.js
@@ -1,26 +1,10 @@
 import React, { useEffect } from 'react';
 import './App.css';
-//import getData from './main';
+import getData from './main';
 
 function App() {
   useEffect(() =>{
-    const urls = [
-      `http://localhost:8000/clothing`,
-      `http://localhost:8000/shoes`,
-      `http://localhost:8000/accessories`
-    ];
-    
-    let requests = urls.map(url => fetch(url));
-
-    Promise.all(requests)
-      .then(responses => {
-        return responses;
-      }).then(responses => Promise.all(responses.map(r => r.json())))
-      .then((response) => {
-        console.log(response);
-        let mergedArray = response.reduce((acc, category) => acc.concat(category), []);
-        console.log(mergedArray);
-      })
+    getData();
   }, [])
   return (
     <div className="App">
---
+++ react-katth/src/main.js
@@ -0,0 +1,20 @@
+const getData = async () => {
+  const urls = [
+    `http://localhost:8000/clothing`,
+    `http://localhost:8000/shoes`,
+    `http://localhost:8000/accessories`
+  ];
+  
+  let requests = urls.map(url => fetch(url));
+
+  Promise.all(requests)
+    .then(responses => {
+      return responses;
+    }).then(responses => Promise.all(responses.map(r => r.json())))
+    .then((response) => {
+      let mergedArray = response.reduce((acc, category) => acc.concat(category), []);
+      return mergedArray;
+    })
+}
+
+export default getData;
