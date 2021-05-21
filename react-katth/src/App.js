5fa08f6e06774bb1680cd8fbec92ff29f5be2f46
Brenda García <brendagar88@gmail.com>
2021-05-20 23:45:43 -0500

    Trae toda la data mock

--- react-katth/src/App.js
+++ react-katth/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+//import Mock from './Mock';
 import './App.css';
 
 function App() {
+  let getData = async() => {
+    const url = `http://localhost:8000/clothing`
+    const getFetchData = await fetch(url).then(result => result.json())
+    console.log(getFetchData)
+  }
+  useEffect(() =>{
+    getData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
